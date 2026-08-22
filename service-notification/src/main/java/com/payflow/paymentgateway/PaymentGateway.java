55f6b6d984a3ce1f913b4ff34d8165135a89147b
Bima Satrya Sebayang <34059058+BimaSebayang@users.noreply.github.com>
2026-08-22 16:12:42 +0700

    refactor : Remove Unused Folder.

--- service-notification/src/main/java/com/payflow/paymentgateway/PaymentGateway.java
+++
@@ -1,8 +0,0 @@
-package com.payflow.paymentgateway;
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-@SpringBootApplication
-public class PaymentGateway {
- public static void main(String[] args){
-   SpringApplication.run(com.payflow.paymentgateway.PaymentGateway.class,args);
- }}
--- service-notification/src/main/java/com/payflow/paymentgateway/shared/enums/Currency.java
+++
@@ -1,5 +0,0 @@
-package com.payflow.paymentgateway.shared.enums;
-
-public enum Currency {
-    IDR
-}
